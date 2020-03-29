010b02debca9f98ba6c37e8f85d2929fe037f130
Acácio Correia <acaciofilipe.correia@gmail.com>
2020-03-29 17:02:55 +0100

    Fixed issue when there's a bad response from OMDB

--- processing.py
+++ processing.py
@@ -88,7 +88,7 @@ def search_show_information_by_type(search_text, show_type):
         show = json.loads(show_json)
 
         # When the omdb can't find the information
-        if not show['Response']:
+        if show['Response'] == 'False':
             results.append(show_dict)
             continue
 
