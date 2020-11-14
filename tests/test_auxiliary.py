27ed0ee338ae0163fc0ca1eebef65e4015f46c94
Acácio Correia <acaciofilipe.correia@gmail.com>
2020-11-14 11:45:35 +0000

    Added first unit tests for auxiliary.py

---
+++ tests/test_auxiliary.py
@@ -0,0 +1,34 @@
+import unittest
+import auxiliary
+
+
+class TestAuxiliary(unittest.TestCase):
+    def test_strip_accents_01(self):
+        """ Test the function strip_accents. """
+
+        # The expected result
+        expected_result = 'aaaaaAAAAAoooOOOeeeEEErt'
+
+        # Call the function
+        text = 'aàáãâAÁÀÃÂoóôOÓÔeéêEÉÊrt'
+        actual_result = auxiliary.strip_accents(text)
+
+        # Verify the result
+        self.assertEqual(expected_result, actual_result)
+
+    def test_remove_quote_01(self):
+        """ Test the function remove_quote. """
+
+        # The expected result
+        expected_result = 'schits, schits, schits'
+
+        # Call the function
+        text = 'schit\'s, schit`s, schit´s'
+        actual_result = auxiliary.remove_quote(text)
+
+        # Verify the result
+        self.assertEqual(expected_result, actual_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
