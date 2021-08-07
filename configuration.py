33fa5ca97f1bbfb17bd198a2f0658d391a679c3c
Acácio Correia <acaciofilipe.correia@gmail.com>
2021-08-07 13:44:50 +0100

    Trying to fix DB issues

--- configuration.py
+++ configuration.py
@@ -80,7 +80,7 @@ def initialize():
         print('Warning: Unable to find database url!')
         exit(1)
 
-    engine = create_engine(database_url, encoding='utf-8', pool_recycle=280)
+    engine = create_engine(database_url, encoding='utf-8', pool_recycle=3600, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
 
     MIGRATIONS_DIR = os.path.join(base_dir, 'migrations')
